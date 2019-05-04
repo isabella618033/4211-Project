9f799aadc8f06520424653ae33fbac9fe6a9d58b
isabella618033 <49876827+isabella618033@users.noreply.github.com>
2019-05-05 00:30:39 +0800

    Add files via upload
    1. must download both Cleaning the Crew.ipynb, CleaningFunctions.py to work
    #dropping the columns that was not needed
    #filling empty list with []
    #changing a string of list into list
    #changing the dict in the list into ID only
    # getting all unique values
    # adding the extra columns for the most related crew (improved on timing!!! now only takes ~30sec!!!)
    #changing the time into numerical value (improved on timing!!! now only takes ~30sec!!!)
    # save to save.csv
    #dropping the extra columns that was already extracted
    #creating df df_pred to do prediction for those with non-nan values > 2000
    #cause knn dosent accept a string heading, so changng the heading
    # KNN avaliable
    #adding back the columns for df_pred
    #merging df with df_pred

---
+++ CleaningFunctions.py
@@ -0,0 +1,89 @@
+import pandas as pd 
+import ast
+from ast import literal_eval
+import numpy as np
+from pandas import DataFrame
+from IPython.display import display, clear_output
+import math
+
+from datetime import datetime
+from dateutil.parser import parse
+import time
+import datetime
+
+def worker(x):
+    return x*x
+
+def Getting3MostRelated(df,col):
+    df_temp = pd.DataFrame()
+    print(col)
+    #collect all revenue for each crew
+    Rev_List = {}
+    for x in range(len(df)):
+        if not (isinstance(df[col][x], float )):
+            for y in range( len(df[col][x])):
+                if (df[col][x][y] not in Rev_List):
+                    list = []
+                    Rev_List.update( {df[col][x][y]: list} )
+                Rev_List[df[col][x][y]].append(df["revenue"][x])
+                
+    #calculate the variance of revenue for each crew
+    var = {}
+    for id in Rev_List.keys():
+        if (len(Rev_List[id]) > 2):
+            var.update({id : np.var(Rev_List[id])})
+        else : 
+            var.update({id : np.nan})
+
+    #creat the 3 new columns
+    length = len(df)
+    data = {col+str(0):range(length), col+str(1):range(length),col+str(2):range(length)} 
+    df_temp = pd.DataFrame(data) 
+
+    # get the value for each 3 columns
+    for x in range(len(df)):
+        if (True): #check if we have already ran throught this column before
+            sub_var = {}
+            print(x, col, df[col][x], type(df[col][x]),isinstance(df[col][x], float))
+            if not isinstance(df[col][x], float): #if the df is not nan for col,x
+                print("len(df[col][x])",len(df[col][x]))
+                for id in df[col][x]:
+                    sub_var.update({id : var[id]}) # get the list of variance for each crew in this cell
+                #print(sub_var)
+                for a in range(3):
+                    name = col +str(a)
+                    if(len(sub_var)>0):
+                        min_item = min(sub_var.keys(), key=(lambda k: sub_var[k]))
+                        df_temp[name][x] = float(min_item)
+                        sub_var.pop(min_item)
+                    else:
+                        df_temp[name][x] = np.nan
+    return df_temp, col
+
+def job(num):
+    data = [['tom', num+1], ['nick', num+2], ['juli', num+3]] 
+    df = pd.DataFrame(data, columns = ['Name', 'Age']) 
+
+    return df
+
+def time2num(df, count):
+    x = datetime.datetime(1970, 1, 1)
+    helo_time=time.time()
+    date = df.release_date[count]
+    if (isinstance(date, str))and(date[-3] == "/"):
+        if(int(date[-2], 10) < 2):
+            df.release_date[count] = date[:-2] + '20' + date[-2:]
+        else:
+            df.release_date[count] = date[:-2] + '19' + date[-2:]
+        
+    if (isinstance(date, str)):
+        df.release_date[count] = datetime.datetime.strptime(df.release_date[count], '%m/%d/%Y')
+
+    if  isinstance(df.release_date[count], datetime.datetime):
+        #clear_output(wait=True)
+        #display(str(count)+ "           "+str(df.release_date[count]))
+        if(df.release_date[count].year >= 1970):
+            df.release_date[count] =(df.release_date[count]-x).days + 365.25*15
+        else:
+            df.release_date[count] = (df.release_date[count].year-1955)*365.25 + (df.release_date[count].month-1)*(365.25/12)+(df.release_date[count].day-1)
+    return df.release_date[count]
